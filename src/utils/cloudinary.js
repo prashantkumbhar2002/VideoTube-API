3d86aa675c76e5dacff62637de92bc5044aa2e78
prashantkumbhar2002 <prashantkumbhar5515@gmail.com>
2024-02-17 02:13:38 +0530

    Added the middleware functions and jwt sign and compare method in the user model
     * Added the cloudinary file u[load util
    
     * Added the multer middleware to upload the file to the server and then to the cloudinary and after successful upload to the cloudinary remove it.

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+
+const uploadOnCloudinary = async (localpath) => {
+    try{
+        if(!localpath) return null;
+        //upload the file on the cloudinary
+        const response = await cloudinary.uploader.upload(localpath, { resource_type: "auto"});
+        console.log("File uploaded on the cloudinary: ", response.url); 
+        return response;
+    }
+    catch(error){
+        fs.unlinkSync(localpath);       //remove the file from the server
+        return null   
+    }
+}
+
+
+export { uploadOnCloudinary };
+
