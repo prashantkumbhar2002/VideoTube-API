ee7bb00a2a14a1fdf7ddae3f9808fdc0b6e073b1
prashantkumbhar2002 <prashantkumbhar5515@gmail.com>
2024-02-20 01:11:28 +0530

    Added the route and controller for the users model

--- src/app.js
+++ src/app.js
@@ -11,4 +11,14 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+//import routes
+
+import userRouter from './routes/user.routes.js';
+
+
+//route definition 
+app.use("/api/v1/users", userRouter)
+
 export default app;
