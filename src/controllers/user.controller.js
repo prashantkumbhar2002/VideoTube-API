b836cdbb013d68e9f27be420c3cd32f51294c6f1
prashantkumbhar2002 <prashantkumbhar5515@gmail.com>
2024-03-03 17:28:37 +0530

    Added controller for fetching user Profile page details like subscribers count, subscribedTo, isSubscribed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -334,4 +334,77 @@ const updateCoverImage = asyncHandler(async(req, res) => {
         )
     )
 })
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateAvatar, updateCoverImage};
+
+
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+    const {userName} = req.params;
+    if(!userName.trim()){
+        throw new APIError(400, "UserName is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                userName: userName?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                subscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                userName: 1,
+                subscribersCount: 1,
+                subscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new APIError(404, "Channel does not exist")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new APIResponse(
+            200, channel[0], "User channel fetched successfully"
+        )
+    )
+})
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateAvatar, updateCoverImage, getUserChannelProfile};
