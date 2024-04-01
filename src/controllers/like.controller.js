067875b80b4613efa4e9872d2734236ad606b436
prashantkumbhar2002 <prashantkumbhar5515@gmail.com>
2024-04-01 22:59:08 +0530

    Added toggleTweetLike and getLikedVideos

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,6 +4,7 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { APIError } from "../utils/apiError.js"
 import { APIResponse } from "../utils/apiResponse.js"
 import { Video } from "../models/video.model.js"
+import { Tweet } from "../models/tweet.model.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
@@ -68,11 +69,86 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
-}
-)
+    if(!tweetId || isValidObjectId(tweetId)){
+        throw new APIError(200, "Invalid tweetId")
+    }
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet){
+        throw new APIError(400, "Tweet not found")
+    }
+    const alreadyLiked = await Like.findOne({
+        tweet: tweetId,
+        likedBy: req.user?._id
+    });
+    if(alreadyLiked){
+        await Like.findByIdAndDelete(alreadyLiked?._id)
+        return res
+            .status(200)
+            .json( new APIResponse(200, {isLiked: false}))
+    }
+    await Like.create({
+        tweet: tweetId,
+        likedBy: req.user?._id,
+    })
+    return res
+        .status(200)
+        .json(new APIResponse(200, {isLiked: true}))
+})
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const likedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy : new mongoose.Types.ObjectId(req.user?._id) 
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideos",
+                pipeline: [
+                    {
+                        $pipeline: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "ownerDetails"
+                        }
+                    },
+                    {
+                        $unwind: "$ownerDetails"
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$likedVideos",
+        },
+        {
+            $project: {
+                _id: 1,
+                videoFile: 1,
+                thumbnail: 1,
+                title: 1,
+                description: 1,
+                views: 1,
+                duration: 1,
+                createdAt: 1,
+                isPublished: 1,
+                ownerDetails: {
+                    userName: 1,
+                    fullName: 1,
+                    avatar: 1
+                }
+            }
+        }
+    ]);
+    return res
+        .status(200)
+        .json(new APIResponse(200, likedVideos, "Liked videos fetched successfully"))
 })
 
 export {
